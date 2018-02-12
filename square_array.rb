cb426abe1ca8f29b8c9c08eec25cf374dd4c434b
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 18:49:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def square_array(numbers)
   square_array = []
     numbers.each do |number|
         squre = numbers * number
       square_array.push(square)
+      binding.pry
   end
   return square_array
 end
