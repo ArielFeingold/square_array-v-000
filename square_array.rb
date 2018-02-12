488cc2a5d80e7211d4887411df71ca41261fa997
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 18:30:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
-def square_array(array)
-  array.each do |number|
+def square_array(numbers)
+  square_array.each do |number|
     new_array =  []
     new_array << number**
   end
   new_array
-end
 end
